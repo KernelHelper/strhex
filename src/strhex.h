23bcfe495148c29cd368f4691e0c85ad9031427f
xingyun86 <xingyun_ppshuai@yeah.net>
2019-01-13 16:00:50 +0800

    提交strhex

---
+++ src/strhex.h
@@ -0,0 +1,48 @@
+
+#include <string>
+
+__inline static char s2c(short s)
+{
+	char * a = "\x10\x01\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30___7___\x41\x41\x41\x41\x41\x41____________26____________\x61\x61\x61\x61\x61\x61\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00___7___\x0A\x0A\x0A\x0A\x0A\x0A____________26____________\x0a\x0a\x0a\x0a\x0a\x0a";
+	char c = 0;
+	for (int i = 0; i < sizeof(s); i++)
+	{
+		c += a[i] * (((char *)&s)[i] - a[((char *)&s)[i] - '0' + sizeof(short)] + a[((char *)&s)[i] - '0' + ('f' - '0') + sizeof(char) + sizeof(short)]);
+	}
+	return c;
+}
+__inline static short c2s(char c)
+{
+	char * a = "\xF0\x0F\x04\x00\x30\x31\x32\x33\x34\x35\x36\x37\x38\x39\x41\x42\x43\x44\x45\x46";
+	short s = 0;
+	for (int i = 0; i < sizeof(s); i++)
+	{
+		((char*)&s)[i] = a[((unsigned char)(c & a[i]) >> a[i + sizeof(short)]) + sizeof(short) + sizeof(short)];
+	}
+
+	return s;
+}
+__inline static std::string str2hex(std::string s)
+{
+	std::string h(s.length() * sizeof(short), '\0');
+	for (int i = 0; i < s.length(); i++)
+	{
+		((short*)h.c_str())[i] = c2s(s.at(i));
+	}
+	return h;
+}
+__inline static std::string hex2str(std::string h)
+{
+	std::string s(h.length() / sizeof(short), '\0');
+	for (int i = 0; i < s.length(); i++)
+	{
+		s.at(i) = s2c(((short*)h.c_str())[i]);
+	}
+	return s;
+}
+__inline static std::string str_xor(std::string s)
+{
+	std::string x(s);
+	for (auto c : x){ c ^= (unsigned char)(0xFF); }
+	return x;
+}
