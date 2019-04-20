e03ededc97f3262e2ad53e86f4e0250cd2e46d08
xingyun86 <xingyun_ppshuai@yeah.net>
2019-04-20 21:05:28 +0800

    提交字符串转十六进制字符串的新实现

--- src/strhex.h
+++ src/strhex.h
@@ -3,7 +3,7 @@
 
 __inline static char s2c(short s)
 {
-	char * a = "\x10\x01\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30___7___\x41\x41\x41\x41\x41\x41____________26____________\x61\x61\x61\x61\x61\x61\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00___7___\x0A\x0A\x0A\x0A\x0A\x0A____________26____________\x0a\x0a\x0a\x0a\x0a\x0a";
+	const char * a = "\x10\x01\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30___7___\x41\x41\x41\x41\x41\x41____________26____________\x61\x61\x61\x61\x61\x61\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00___7___\x0A\x0A\x0A\x0A\x0A\x0A____________26____________\x0a\x0a\x0a\x0a\x0a\x0a";
 	char c = 0;
 	for (int i = 0; i < sizeof(s); i++)
 	{
@@ -13,7 +13,7 @@ __inline static char s2c(short s)
 }
 __inline static short c2s(char c)
 {
-	char * a = "\xF0\x0F\x04\x00\x30\x31\x32\x33\x34\x35\x36\x37\x38\x39\x41\x42\x43\x44\x45\x46";
+	const char * a = "\xF0\x0F\x04\x00\x30\x31\x32\x33\x34\x35\x36\x37\x38\x39\x41\x42\x43\x44\x45\x46";
 	short s = 0;
 	for (int i = 0; i < sizeof(s); i++)
 	{
@@ -22,16 +22,16 @@ __inline static short c2s(char c)
 
 	return s;
 }
-__inline static std::string str2hex(std::string s)
+__inline static std::string hex2str(std::string s)
 {
-	std::string h(s.length() * sizeof(short), '\0');
-	for (int i = 0; i < s.length(); i++)
+	std::string h(s.size() * sizeof(short), '\0');
+	for (int i = 0; i < s.size(); i++)
 	{
 		((short*)h.c_str())[i] = c2s(s.at(i));
 	}
 	return h;
 }
-__inline static std::string hex2str(std::string h)
+__inline static std::string str2hex(std::string h)
 {
 	std::string s(h.length() / sizeof(short), '\0');
 	for (int i = 0; i < s.length(); i++)
@@ -49,21 +49,135 @@ __inline static std::string str_xor(std::string s)
 __inline static std::string hex_to_str(std::string h)
 {
 	std::string s((""));
-	char ch[3] = { 0 };
-	for (auto c : h) { sprintf(ch, ("%02X"), (unsigned char)c);s.append(ch); }
+	for (auto c : h) {char ch[sizeof(short) + 1] = { 0 }; sprintf_s(ch, ("%02X"), (unsigned char)c);s.append(ch); }
 	return s;
 }
 __inline static std::string str_to_hex(std::string s)
 {
 	std::string h((""));
-	size_t i = 0;
-	size_t l = s.length();
-	if (!(l % sizeof(short)))
+	for (size_t i = 0; i < s.length(); i += sizeof(short))
 	{
-		for (i = 0; i < l; i += sizeof(short))
-		{
-			h.push_back((unsigned char)strtoul(s.substr(i, sizeof(short)).c_str(), NULL, 0x10));
-		}
+		h.push_back((unsigned char)(int)std::strtoul(s.substr(i, sizeof(short)).c_str(), NULL, 0x10));
 	}
 	return h;
+}
+#include <sstream>
+
+std::string str_to_hex_2(const std::string& str)
+{
+	std::stringstream ss;
+	const std::string hex = ("0123456789ABCDEF");
+	for (std::size_t i = 0; i < str.size(); i++)
+	{
+		ss << hex[(unsigned char)str[i] >> 4] << hex[(unsigned char)str[i] & 0xF];
+	}
+	return ss.str();
+}
+
+std::string hex_to_str_2(const std::string& hex)
+{
+	std::string result = ("");
+	for (std::size_t i = 0; i < hex.length(); i += sizeof(short))
+	{
+		result.push_back((unsigned char)(int)std::strtol(hex.substr(i, sizeof(short)).c_str(), NULL, 0x10));
+	}
+	return result;
+}
+
+__inline static void hex_str(unsigned char *inchar, unsigned int len, unsigned char *outtxt)
+{
+	unsigned int i = 0x00;
+	unsigned char hbit = 0x00, lbit = 0x00;
+	for (i = 0x00;i < len;i++)
+	{
+		hbit = (*(inchar + i) & 0xF0) >> 0x04;
+		lbit = *(inchar + i) & 0x0F;
+		if (hbit > 0x09)
+		{
+			outtxt[0x02 * i] = 'A' + hbit - 0x0A;
+		}
+		else
+		{
+			outtxt[0x02 * i] = '0' + hbit;
+		}
+		if (lbit > 0x09)
+		{
+			outtxt[0x02 * i + 1] = 'A' + lbit - 0x0A;
+		}
+		else
+		{
+			outtxt[0x02 * i + 1] = '0' + lbit;
+		}
+	}
+	outtxt[0x02 * i] = 0x00;
+}
+__inline static unsigned int str_hex(unsigned char *str, unsigned char *hex)
+{
+	unsigned int num = 0x00;
+	unsigned char ctmp = 0x00, ctmp1 = 0x00, half = 0x00;
+	do
+	{
+		do
+		{
+			half = 0x00;
+			ctmp = *str;
+			if (!ctmp)
+			{
+				break;
+			}
+			str++;
+		} while ((ctmp == 0x20) || (ctmp == 0x2c) || (ctmp == '\t'));
+		if (!ctmp)
+		{
+			break;
+		}
+		if (ctmp >= 'a')
+		{
+			ctmp = ctmp - 'a' + 0x0A;
+		}
+		else if (ctmp >= 'A')
+		{
+			ctmp = ctmp - 'A' + 0x0A;
+		}
+		else
+		{
+			ctmp = ctmp - '0';
+		}
+		ctmp = ctmp << 0x04;
+		half = 0x01;
+		ctmp1 = *str;
+		if (!ctmp1)
+		{
+			break;
+		}
+		str++;
+		if ((ctmp1 == 0x20) || (ctmp1 == 0x2c) || (ctmp1 == '\t'))
+		{
+			ctmp = ctmp >> 0x04;
+			ctmp1 = 0x00;
+		}
+		else if (ctmp1 >= 'a')
+		{
+			ctmp1 = ctmp1 - 'a' + 0x0A;
+		}
+		else if (ctmp1 >= 'A')
+		{
+			ctmp1 = ctmp1 - 'A' + 0x0A;
+		}
+		else
+		{
+			ctmp1 = ctmp1 - '0';
+		}
+		ctmp += ctmp1;
+		*hex = ctmp;
+		hex++;
+		num++;
+	} while (1);
+	if (half)
+	{
+		ctmp = ctmp >> 0x04;
+		*hex = ctmp;
+		num++;
+	}
+	return(num);
 }
